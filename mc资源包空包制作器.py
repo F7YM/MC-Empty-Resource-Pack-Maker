5ec2dc3674be73231f3a90b4616509b715fbd177
F7YM <130836015+F7YM@users.noreply.github.com>
2023-07-28 17:26:06 +0800

    Update to v1.1.0

--- mc资源包空包制作器.py
+++ mc资源包空包制作器.py
@@ -4,18 +4,28 @@ import os
 uuid1 = str(uuid.uuid4())
 uuid2 = str(uuid.uuid4())
 try:
-    os.mkdir("Tutorial_Resource_Pack")
+    os.mkdir('Tutorial_Resource_Pack')
 except:
-    print("创建Tutorial_Resource_Pack文件夹失败，请检查是否已有重名文件夹")
+    print('创建Tutorial_Resource_Pack文件夹失败，请检查是否已有重名文件夹')
     exit()
 
-name = input("包名：")
-description = input("包描述：")
+name = input('包名：')
+description = input('包描述：')
+easytocreate = input('[非正式包可不设置]是否设置包版本号(y/n)：')
 
-with open("Tutorial_Resource_Pack/manifest.json","w",encoding="utf-8") as f:
-    f.write("")
+if easytocreate == 'y':
+    version1 = input('[必填整数数字,不可为空]大版本号(格式为 大.中.小 版本号，如1.0.0)：')
+    version2 = input('[必填整数数字,不可为空]中版本号(格式为 大.中.小 版本号，如1.0.0)：')
+    version3 = input('[必填整数数字,不可为空]小版本号(格式为 大.中.小 版本号，如1.0.0)：')
+else:
+    version1 = '0'
+    version2 = '0'
+    version3 = '0'
 
-with open("Tutorial_Resource_Pack/manifest.json","a",encoding="utf-8") as f:
+with open('Tutorial_Resource_Pack/manifest.json','w',encoding='utf-8') as f:
+    f.write('')
+
+with open('Tutorial_Resource_Pack/manifest.json','a',encoding='utf-8') as f:
     f.write('''{
   "format_version": 2,
   "header": {
@@ -28,8 +38,15 @@ with open("Tutorial_Resource_Pack/manifest.json","a",encoding="utf-8") as f:
     "uuid": "''')
     f.write(uuid1)
     f.write('''",
-    "version": [1, 0, 0],
-    "min_engine_version": [1, 13, 0]
+    "version": [''')
+    f.write(version1)
+    f.write(', ')
+    f.write(version2)
+    f.write(', ')
+    f.write(version3)
+    f.write('''],
+    ''')
+    f.write('''"min_engine_version": [1, 13, 0]
   },
   "modules": [
     {
@@ -40,24 +57,30 @@ with open("Tutorial_Resource_Pack/manifest.json","a",encoding="utf-8") as f:
       "uuid": "''')
     f.write(uuid2)
     f.write('''",
-      "version": [1, 0, 0]
-    }
+      "version": [''')
+    f.write(version1)
+    f.write(''', ''')
+    f.write(version2)
+    f.write(''', ''')
+    f.write(version3)
+    f.write(''']''')
+    f.write('''}
   ]
 }''')
-os.mkdir("Tutorial_Resource_Pack/textures")
-os.mkdir("Tutorial_Resource_Pack/textures/entity")
-os.mkdir("Tutorial_Resource_Pack/textures/blocks")
-os.mkdir("Tutorial_Resource_Pack/textures/items")
-os.mkdir("Tutorial_Resource_Pack/textures/environment")
-os.mkdir("Tutorial_Resource_Pack/sounds")
-os.mkdir("Tutorial_Resource_Pack/sounds/music")
-os.mkdir("Tutorial_Resource_Pack/texts")
-os.mkdir("Tutorial_Resource_Pack/animation_controllers")
-os.mkdir("Tutorial_Resource_Pack/animations")
-os.mkdir("Tutorial_Resource_Pack/animations/models")
-os.mkdir("Tutorial_Resource_Pack/attachables")
-os.mkdir("Tutorial_Resource_Pack/models")
-os.mkdir("Tutorial_Resource_Pack/models/entity")
-os.mkdir("Tutorial_Resource_Pack/models/blocks")
-os.mkdir("Tutorial_Resource_Pack/render_controllers")
-print("Done.")
+os.mkdir('Tutorial_Resource_Pack/textures')
+os.mkdir('Tutorial_Resource_Pack/textures/entity')
+os.mkdir('Tutorial_Resource_Pack/textures/blocks')
+os.mkdir('Tutorial_Resource_Pack/textures/items')
+os.mkdir('Tutorial_Resource_Pack/textures/environment')
+os.mkdir('Tutorial_Resource_Pack/sounds')
+os.mkdir('Tutorial_Resource_Pack/sounds/music')
+os.mkdir('Tutorial_Resource_Pack/texts')
+os.mkdir('Tutorial_Resource_Pack/animation_controllers')
+os.mkdir('Tutorial_Resource_Pack/animations')
+os.mkdir('Tutorial_Resource_Pack/animations/models')
+os.mkdir('Tutorial_Resource_Pack/attachables')
+os.mkdir('Tutorial_Resource_Pack/models')
+os.mkdir('Tutorial_Resource_Pack/models/entity')
+os.mkdir('Tutorial_Resource_Pack/models/blocks')
+os.mkdir('Tutorial_Resource_Pack/render_controllers')
+print('Done.')
